9548909f965479a68dd665be729db06b8fe33cd7
nanda <nandagolla074@gmail.com>
2025-07-10 17:12:55 +0530

    terraform module vpc modifications

--- 00-vpc/outputs.tf
+++ 00-vpc/outputs.tf
@@ -1,3 +1,15 @@
 # output "azs" {
 #   value = module.vpc.available_zones
 # }
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_list
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_list
+}
+
+output "database_subnet_ids" {
+  value = module.vpc.database_subnet_list
+}
