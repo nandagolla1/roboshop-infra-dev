f1213a44f8ffe7bb16cd90dc354a62daf41c0375
nanda <nandagolla074@gmail.com>
2025-07-18 23:28:45 +0530

    terraform batabases

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -33,3 +33,17 @@ resource "aws_lb_listener" "backend_alb_listener" {
     }
   }
 }
+
+
+resource "aws_route53_record" "backend_alb" {
+  zone_id = var.zone_id
+  name    = "*.backend-dev.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = module.backend_alb.dns_name
+    zone_id                = module.backend_alb.zone_id
+    evaluate_target_health = true
+  }
+  allow_overwrite = true
+}
--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -35,3 +35,11 @@ variable "backend_alb_sg_description" {
   type = string
   default = "created sg for backend alb instance."
 }
+
+variable "zone_id" {
+  default = "Z0241085GWXCWOXHL9YW"
+}
+
+variable "domain_name" {
+  default = "nanda.cyou"
+}
