f298ae35d81dc125de62a3e324eb6159d750bee7
nanda <nandagolla074@gmail.com>
2025-07-27 00:03:47 +0530

    terraform batabases

--- 50-backend-alb/parameters.tf
+++ 50-backend-alb/parameters.tf
@@ -2,5 +2,5 @@ resource "aws_ssm_parameter" "backend_alb_listener_arn" {
   name  = "/${var.project}/${var.environment}/${var.backend_alb_sg_name}"
   type  = "String"
   value = module.catalogue.sg_id
-  depends_on = [ module.catalogue ]
+  depends_on = [ module.backend_alb ]
 }
