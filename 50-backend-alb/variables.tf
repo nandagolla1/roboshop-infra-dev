643811ef583af5836e5343f6cb91f181cce3dc21
maithili <maithiligolla@gmail.com>
2025-08-01 15:02:36 +0530

    terraform batabases

--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -6,26 +6,6 @@ variable "environment" {
   default = "dev"
 }
 
-variable "frontend_sg_name" {
-    type = string
-  default = "frontend"
-}
-
-variable "frontend_sg_description" {
-  type = string
-  default = "created sg for frontend instance."
-}
-
-variable "bastion_sg_name" {
-    type = string
-  default = "bastion"
-}
-
-variable "bastion_sg_description" {
-  type = string
-  default = "created sg for bastion instance."
-}
-
 variable "backend_alb_sg_name" {
     type = string
   default = "backend_alb"
